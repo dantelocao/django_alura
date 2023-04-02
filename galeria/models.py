3abc09df14cbeafca6a5733d8a7fae6ad7808302
Dante <dante.dardaque@hotmail.com>
2023-04-02 14:31:00 -0300

    tesde de novo

---
+++ galeria/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    desc = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
+# Create your models here.
---
+++ galeria/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+
+    dados = {
+        1: {"Nome": "Nebulosa de Carina",
+            "Legenda": "webbtelescope.org / NASA / James Webb"},
+        2: {"Nome": "Galáxia NGC 1079",
+            "Legenda": "nasa.org / NASA / Hubble"}
+    }
+    return render(request, 'galeria\index.html', {"cards": dados})
+
+
+def imagem(request):
+    return render(request, 'galeria\imagem.html')
